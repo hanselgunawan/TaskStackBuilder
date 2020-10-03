9692725bcfe64e72b3d80413ec7607e29429fc4a
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-10-03 04:30:52 -0700

    added taskstackbuilder

--- app/src/main/java/com/hanseltritama/taskstackbuilderdemo/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/taskstackbuilderdemo/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.hanseltritama.taskstackbuilderdemo
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_DEFAULT
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+
+        val detailActivity = Intent(this, DetailsActivity::class.java)
+        val stackBuilder = TaskStackBuilder.create(this)
+        stackBuilder.addNextIntentWithParentStack(detailActivity)
+        val pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val notification: Notification = NotificationCompat.Builder(this, "channelId")
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("Notification Test")
+            .setContentText("Hey, This is just a test.")
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        button.setOnClickListener {
+            notificationManager.notify(0, notification)
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "test_channel"
+            val descriptionText = "this is my test channel"
+            val importance = IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("channelId", name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                application.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
